be6c20865a651a1320acfd9a1a0bac80f065a63f
DJ Roychoudhury and Jeffrey Hackford <dj@>
2016-05-31 13:31:10 -0700

    add edit description option to tasks

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,13 +1,22 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
+import {EditTaskDetailsComponent} from './edit-task-details.component';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent],
-  templateUrl: 'app/task-list.component.html'
+  directives: [TaskComponent, EditTaskDetailsComponent],
+  template: `
+  <task-display *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
+  <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
+  </edit-task-details>
+  `
 })
 
 export class TaskListComponent {
